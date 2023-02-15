ca3ecee8b3e73615d9fc7740104073873f1276da
YasminGon <yasmingon08@hotmail.com>
2023-02-15 17:14:08 +0000

    salvando arquivo

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,5 +1,33 @@
 public class EstruturaDeRepeticao {
 public static void mais(String[]Yasmin){
+    //Exemplo1       
+    /*for(int i=0; i<7;i++){           
+        System.out.println(i);       
+        }*/
+        
+    //atividade1        
+    /*for(int i=150; i<=300;i++){           
+    System.out.println(i);        
+    }*/
+            
+     //Atividade2       
+    /*int soma=0;        
+    for(int i=1; i<=1000;i++){            
+    soma=soma + i;        
+    }        
+    System.out.println(soma);*/
+            
+    //Atividade3        
+    /*for(int i=3; i<100;i+=3){            
+    System.out.println(i);        
+    }*/
+                
+    int fat=1;
+    for(int i=1; i<10;i++){
+        fat *= i;
+        }
+    System.out.println(fat); 
 
-}
-}
+     }  
+  }
+
